0978944eb272369f9c8997c23069c21751cde83b
Arsh <arshkr07@gmail.com>
2026-01-10 19:12:45 +0100

    Initial Commit

---
+++ Helix/HelixApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelixApp.swift
+//  Helix
+//
+//  Created by Arsh Kumar on 10.01.26.
+//
+
+import SwiftUI
+
+@main
+struct HelixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
